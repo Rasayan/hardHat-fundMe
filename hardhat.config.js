654f3cc0232759852ac7accf9588a61a15707fe4
razayn <razayn@kali.razayn>
2024-03-02 12:38:26 +0530

    added mock contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -32,11 +32,13 @@ module.exports = {
       {
         version: "0.8.24",
       },
+      {
+        version: "0.6.6",
+      },
     ],
   },
   etherscan: {
     apiKey: ETH_API_KEY,
-    // customChains: [], // uncomment this line if you are getting a TypeError: customChains is not iterable
   },
   gasReporter: {
     enabled: true,
@@ -47,8 +49,8 @@ module.exports = {
   },
   namedAccounts: {
     deployer: {
-      default: 0, // here this will by default take the first account as deployer
-      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+      default: 0,
+      1: 0,
     },
   },
   mocha: {
